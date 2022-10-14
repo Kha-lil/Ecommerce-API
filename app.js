4a4f761a2b81ea9897b6c9df202ac9e758522362
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-10-14 11:01:11 +0100

    chore: app.js Installed dependencies, wrote code for the basic server boilerplate and also connected to the database.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+//db
+const connectDB = require('./db/connect')
+
+const port = process.env.PORT || 3000;
+
+const start = async (url) => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+  app.listen(port, (req, res) => {
+    console.log(`App is listening on ${port}`);
+  });
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+start();
