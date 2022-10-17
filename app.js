572d618d82f2e8ad6094d4df58c35805d5b92eb1
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-10-17 13:23:54 +0100

    modified: app.js

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 require("dotenv").config();
 require("express-async-errors");
 
+//express
 const express = require("express");
 const app = express();
 
+//other middleware imports
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
 //db
-const connectDB = require('./db/connect')
+const connectDB = require("./db/connect");
+
+//routers
+const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+
+//middlewares
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.get("/", (req, res) => {
+  res.send(`E-commerce API home page`);
+});
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
 const start = async (url) => {
   try {
-    await connectDB(process.env.MONGO_URI)
-  app.listen(port, (req, res) => {
-    console.log(`App is listening on ${port}`);
-  });
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, (req, res) => {
+      console.log(`App is listening on ${port}`);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
