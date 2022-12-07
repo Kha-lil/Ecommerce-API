ba6cbe89a3a15153534e5bbfd1238b0dd24bd88d
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-12-07 02:33:07 +0100

    feat: image upload functionalty complete

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const path = require("path");
 
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId;
@@ -46,7 +47,26 @@ const deleteProduct = async (req, res) => {
 };
 
 const uploadImage = async (req, res) => {
-  res.send("uploadImage route");
+  console.log(req.files);
+  if (!req.files) {
+    throw new CustomError.BadRequestError(`No file uploaded`);
+  }
+  const productImage = req.files.image;
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError(`Please upload an Image`);
+  }
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      `Please upload an image less than 1MB`
+    );
+  }
+  const imagePath = path.resolve(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  productImage.mv(imagePath);
+  res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
 };
 
 module.exports = {
