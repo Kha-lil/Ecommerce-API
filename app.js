220c23997ca06d370c5e7d7e7c71ec72374af909
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-12-07 02:35:53 +0100

    import required modules, dependencies & routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 //other middleware imports
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 //db
 const connectDB = require("./db/connect");
@@ -15,6 +16,8 @@ const connectDB = require("./db/connect");
 //routers
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 
 //middlewares
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -24,19 +27,24 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
+app.use(express.static("./public"));
+app.use(fileUpload());
+
 app.get("/", (req, res) => {
   res.send(`E-commerce API home page`);
 });
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
-const start = async (url) => {
+const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, (req, res) => {
